283c6f5560e434d5f754d879ce2837e909177cd5
lljjjjjj <1523407063@qq.com>
2021-12-17 04:10:33 +0800

    ？

--- src/components/home/StudentHome.vue
+++ src/components/home/StudentHome.vue
@@ -2,48 +2,46 @@
   <div>
     <div class="headStu">
       <div
-        class="headDiv"
-        style="color: white; font-size: 30px; font-family: 宋体"
+          class="headDiv"
+          style="color: white; font-size: 30px; font-family: 宋体"
       >
         <div class="headLeft">
           <!-- 头像部分 -->
           <div>
             <el-menu
-              :default-active="activeIndex"
-              class="port_box"
-              mode="horizontal"
+                class="port_box"
+                mode="horizontal"
+                @select="handleSelect"
+                :router="true"
             >
-<!--              @select="handleSelect"-->
-              <el-submenu class="sub" index="1">
+              <el-submenu index="1">
                 <template slot="title" class="subtitle">
                   <img src="@/assets/port.png" />
                 </template>
-                <el-menu-item>
-                  <el-button type="text" @click="toset" icon="el-icon-s-tools">
-                    账号设置
-                  </el-button>
-                </el-menu-item>
-                <el-menu-item>
+                <!-- <el-menu-item index="/stuinfoset">
+                  <i class="el-icon-s-tools"> 账号设置 </i>
+                </el-menu-item> -->
+                <el-menu-item index="/login">
                   <el-button
-                    type="text"
-                    @click="logout"
-                    icon="el-icon-switch-button"
+                      type="text"
+                      icon="el-icon-switch-button"
+                      @click="logout()"
                   >
-                    安全退出
+                    Log out
                   </el-button>
                 </el-menu-item>
               </el-submenu>
             </el-menu>
           </div>
           <!-- 信息 -->
-          <div>Hi!{{ name }}同学</div>
+          <div>Hi! {{ name }}</div>
         </div>
         <div class="headRight">
           <div class="headbox1">
             <div style="font-size: 60px">
-              {{ count }}
+              {{ courseData.length }}
             </div>
-            <div style="font-size: 20px">实验课程/门</div>
+            <div style="font-size: 20px">Courses</div>
           </div>
         </div>
       </div>
@@ -52,49 +50,182 @@
       <div class="mainDiv">
         <el-card class="mainLeft">
           <div class="ing">
-            <div class="title">
-              <h2>进行中的课程</h2>
-            </div>
+            <h2>Current Courses</h2>
             <div class="content">
-              <div class="course1">课程1</div>
-              <div class="course2">课程2</div>
-              <div class="course3">课程3</div>
-              <div class="course4">课程4</div>
+              <cardList ref="listItem" :dataList="courseData">
+                <!-- <el-button @click="formore">查看</el-button> -->
+              </cardList>
             </div>
           </div>
-          <div class="finish"></div>
+          <div class="finish">
+            <h2>Courses Finished</h2>
+            <div class="content">
+              <cardList ref="listItem" :dataList="endCourseData">
+                <!-- <el-button @click="formore">查看</el-button> -->
+              </cardList>
+            </div>
+          </div>
         </el-card>
-        <el-card class="mainRight">????</el-card>
+        <!-- 公告板 -->
+        <!-- <el-card class="mainRight">????</el-card> -->
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import cardList from "@/components/cardList.vue";
 export default {
+  // props: {
+  //   cardList: {
+  //     type: Array,
+  //     default: function () {
+  //       return [
+  //         {
+  //           course_ID: "42024401",
+  //           course_name: "rjgc",
+  //           teacher_name: "fdfd",
+  //           is_student: "0",
+  //         },
+  //         {
+  //           course_ID: "42024401",
+  //           course_name: "rjgc",
+  //           teacher_name: "fdfd",
+  //           is_student: "0",
+  //         },
+  //       ];
+  //     },
+  //   },
+  // },
+  components: {
+    cardList,
+  },
   data() {
     return {
-      student_id:window.sessionStorage.getItem("account_ID"),
-      name: "刘",
+      account_ID: window.sessionStorage.getItem("account_ID"),
+      name: "",
       count: "0",
-      activeIndex:'1',
+      currentPage: 1,
+      pagesize: 8,
+      store: {
+        id: "",
+      },
+      courseData: [],
+      endCourseData: [],
     };
   },
   methods: {
-    logout() {
-      this.$router.push("/login");
+    showForm(mes) {
+      this.$refs.listItem.recordLocation(mes); //告知卡片列表子组件 要求对本页面的点击事件mes进行定位
     },
-    toset() {
-      this.$router.push("/stuinfoset");
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleSizeChange(page_size) {
+      this.pagesize = page_size;
+    },
+    handleCurrentChange(current_page) {
+      this.currentPage = current_page;
+    },
+    getName() {
+      this.store.id = window.sessionStorage.getItem("account_ID");
+      console.log("id:" + this.store.id);
+      console.log("token:" + window.sessionStorage.getItem("token"));
+      let _this = this;
+      this.$axios({
+        url: "/student/get",
+        method: "get",
+        params: {
+          student_ID: this.store.id,
+        },
+        headers: {
+          token: window.sessionStorage.getItem("token"),
+        },
+      })
+          .then(function (res) {
+            console.log(res.data);
+            _this.name = res.data.name;
+          })
+          .catch(function (error) {
+            console.log("Get Nothing!" + error);
+          });
+    },
+    getCourse() {
+      this.store.id = window.sessionStorage.getItem("account_ID");
+      let _this = this;
+      this.$axios({
+        url: "/course/getTotalCourse",
+        method: "get",
+        params: {
+          account_ID: this.store.id,
+        },
+        headers: {
+          token: window.sessionStorage.getItem("token"),
+        },
+      })
+          .then(function (res) {
+            console.log("res.data[0].length:");
+            console.log(res.data[0].length);
+            _this.courseData = res.data;
+          })
+          .catch(function (error) {
+            console.log("Get Nothing!" + error);
+          });
+    },
+    getEndCourse() {
+      this.store.id = window.sessionStorage.getItem("account_ID");
+      let _this = this;
+      this.$axios({
+        url: "/course/getTotalEndCourse",
+        method: "get",
+        params: {
+          account_ID: this.store.id,
+        },
+        headers: {
+          token: window.sessionStorage.getItem("token"),
+        },
+      })
+          .then(function (res) {
+            console.log("res.data[0].length:");
+            console.log(res.data[0].length);
+            _this.endCourseData = res.data;
+          })
+          .catch(function (error) {
+            console.log("Get Nothing!" + error);
+          });
+    },
+    logout() {
+      sessionStorage.clear();
+      this.$router.push("/login");
+      location.reload();
     },
   },
+  created() {
+    this.getName();
+    this.getCourse();
+    this.getEndCourse();
+  },
   mounted() {
-    // console.log(window.sessionStorage.getItem("account_ID"))
-    // console.log(window.sessionStorage.getItem("token"))
-  }
+    // let _this = this;
+    // let taInfo = new FormData();
+    // taInfo.append("account_ID", account_ID);
+    // this.$axios({
+    //   url: "/course/getTotalCourse",
+    //   method: "get",
+    //   data: taInfo,
+    //   headers: {
+    //     token: window.sessionStorage.getItem("token"),
+    //   },
+    // })
+    //   .then(function (response) {
+    //     _this.cardList = response.data;
+    //   })
+    //   .catch(function (error) {
+    //     console.log("Get Nothing!" + error);
+    //   });
+  },
 };
 </script>
-
 
 <style scoped>
 .headStu {
@@ -105,7 +236,7 @@ export default {
   align-items: center;
 }
 .headDiv {
-  width: 70%;
+  width: 80%;
   display: flex;
   justify-content: space-between;
 }
@@ -124,7 +255,7 @@ export default {
   border-bottom: none;
 }
 .el-menu--horizontal > .el-submenu /deep/ .el-submenu__title {
-  /* 去子菜单颜色 */
+  /* 去子菜单标题颜色 */
   height: 100px;
   display: flex;
   align-items: center;
@@ -147,17 +278,23 @@ export default {
 }
 
 .mainStu {
+  position: absolute;
   display: flex;
   justify-content: center;
-  top:220px;
-  z-index: 2;
+  top: 200px;
+  height: 600px;
+  width: 100%;
+  /* z-index: 2; */
 }
-.mainDiv{
-  width: 70%;
+.mainDiv {
+  width: 80%;
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
 }
-.mainleft {
-  width: 50%;
+.content {
+  width: 1000px;
+}
+.block {
+  margin: 80px;
 }
 </style>
